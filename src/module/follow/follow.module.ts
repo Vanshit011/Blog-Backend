cc761d70326deab91fa29049487a05ec9d4f4ab5
Vanshit Thumar <vanshitt@solguruz.com>
2026-04-15 17:01:39 +0530

    feat: implement follow and unfollow functionality with user statistics and notification integration

--- src/module/follow/follow.module.ts
+++ src/module/follow/follow.module.ts
@@ -4,9 +4,10 @@ import { FollowService } from './follow.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Follow } from './entity/follow.entity';
 import { User } from '../user/entity/user.entity';
+import { NotificationModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Follow, User])],
+  imports: [TypeOrmModule.forFeature([Follow, User]), NotificationModule],
   controllers: [FollowController],
   providers: [FollowService],
   exports: [FollowService],
